bbebefb6db19b1fc292cf100e8b349773eb90d0a
Feyko <feykook@gmail.com>
2022-04-10 21:20:42 +0200

    test: add decode benching

---
+++ qoi/decode_bench_test.go
@@ -0,0 +1,22 @@
+package qoi
+
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+func BenchmarkDecode(b *testing.B) {
+	data, err := os.ReadFile("testimage2.qoi")
+	if err != nil {
+		b.Fatalf("Could not open the test image: %v", err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r := bytes.NewReader(data)
+		_, err := Decode(r)
+		if err != nil {
+			b.Fatalf("%v", err)
+		}
+	}
+}
