b24b66f5e28aa384b2d741a0d1bdef3a7ab9342e
Feyko <feykook@gmail.com>
2022-04-14 22:42:28 +0200

    test(encode): basic test

---
+++ qoi/encode_test.go
@@ -0,0 +1,27 @@
+package qoi
+
+import (
+	"github.com/disintegration/imaging"
+	"github.com/stretchr/testify/require"
+	"image"
+	_ "image/png"
+	"os"
+	"testing"
+)
+
+func TestEncode(t *testing.T) {
+	pngFile, err := os.Open("testimage.png")
+	require.NoErrorf(t, err, "Could not read the PNG test image: %v", err)
+	pngImg, _, err := image.Decode(pngFile)
+	require.NoErrorf(t, err, "Could not decode the PNG test image: %v", err)
+	qoiFile, err := os.OpenFile("testencode.qoi", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	require.NoErrorf(t, err, "Could not open the QOI output image: %v", err)
+	err = NewEncoder(qoiFile, pngImg).Encode()
+	require.NoErrorf(t, err, "Could not encode the test image: %v", err)
+	qoiFile, err = os.Open("testencode.qoi")
+	require.NoErrorf(t, err, "Could not open the encoded image: %v", err)
+	qoiImg, err := Decode(qoiFile)
+	require.NoErrorf(t, err, "Could not decode the encoded image: %v", err)
+	imaging.Save(qoiImg, "testencode.png")
+	require.Equalf(t, pngImg, qoiImg, "The image was not encoded properly")
+}
