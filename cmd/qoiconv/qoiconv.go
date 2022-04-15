48ffd2f1d4b402fc4377c53f65a6bf50ba8af3b6
Feyko <feykook@gmail.com>
2022-04-16 00:55:34 +0200

    feat: qoiconv

---
+++ cmd/qoiconv/qoiconv.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/disintegration/imaging"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
+	"os"
+	"qoi/qoi"
+	_ "qoi/qoi"
+	"strings"
+)
+
+const usage = `Usage: qoiconv <infile> <outfile>
+Examples:
+	qoiconv input.png output.qoi
+	qoiconv input.qoi output.png`
+
+func main() {
+	if len(os.Args) != 3 {
+		printUsage()
+		return
+	}
+
+	inputFilename := os.Args[1]
+	outputFilename := os.Args[2]
+
+	inputImg := openImage(inputFilename)
+
+	if !isQOIFilename(outputFilename) {
+		writeGenericImage(inputImg, outputFilename)
+		return
+	}
+
+	writeQOIImage(inputImg, inputFilename)
+}
+
+func printUsage() {
+	fmt.Println(usage)
+}
+
+func openImage(filename string) image.Image {
+	inputImg, err := imaging.Open(filename)
+	checkForUnsupportedFormat(err)
+	if err != nil {
+		log.Fatalf("Could not open the input image: %v", err)
+	}
+	return inputImg
+}
+
+func checkForUnsupportedFormat(err error) {
+	if errors.Is(err, imaging.ErrUnsupportedFormat) {
+		fmt.Println("The only supported formats are png, jpeg, bmp, tiff & qoi")
+		os.Exit(1)
+	}
+}
+
+func isQOIFilename(filename string) bool {
+	return strings.HasSuffix(filename, ".qoi")
+}
+
+func writeGenericImage(img image.Image, outputFilename string) {
+	err := imaging.Save(img, outputFilename)
+	checkForUnsupportedFormat(err)
+	if err != nil {
+		log.Fatalf("Could not save the output image: %v", err)
+	}
+}
+
+func writeQOIImage(img image.Image, outputFilename string) {
+	outputFile, err := os.Create(outputFilename)
+	if err != nil {
+		log.Fatalf("Could not open the output file: %v", err)
+	}
+	err = qoi.Encode(outputFile, img)
+	if err != nil {
+		log.Fatalf("Could not encode the image: %v", err)
+	}
+}
